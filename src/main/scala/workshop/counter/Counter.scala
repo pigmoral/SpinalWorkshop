bc557d8c7e19522de2f3bf8b4e3115bf38ecf968
Junhui Liu <junhui.liu@pigmoral.tech>
2025-06-05 15:06:05 +0800

    Counter: Add counter

--- src/main/scala/workshop/counter/Counter.scala
+++ src/main/scala/workshop/counter/Counter.scala
@@ -10,4 +10,16 @@ case class Counter(width: Int) extends Component {
   }
 
   // TODO define the logic
+  val counter = Reg(UInt(width bits)) init 0
+
+  when (io.clear) {
+    counter := 0
+  } otherwise {
+    counter := counter + 1
+  }
+
+  io.value := counter
+  // io.full := counter === counter.maxValue
+  // io.full := counter === U(counter.range -> True)
+  io.full := counter === (1 << width) - 1
 }

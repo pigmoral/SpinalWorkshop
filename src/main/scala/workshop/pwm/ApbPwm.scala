d7ceb9341789089d2256187960dcc02babdd3c48
Junhui Liu <junhui.liu@pigmoral.tech>
2025-06-05 15:06:28 +0800

    PWM: Add pwm

--- src/main/scala/workshop/pwm/ApbPwm.scala
+++ src/main/scala/workshop/pwm/ApbPwm.scala
@@ -11,28 +11,93 @@ case class ApbConfig(addressWidth : Int,
 
 // APB interface definition
 case class Apb(config: ApbConfig) extends Bundle with IMasterSlave {
-  // TODO define APB signals
+  // Master Drive
+  val PSEL = Bits(config.selWidth bits)
+  val PENABLE = Bool()
+  val PWRITE = Bool()
+  val PADDR = UInt(config.addressWidth bits)
+  val PWDATA = Bits(config.dataWidth bits)
+
+  // Slave Drive
+  val PRDATA = Bits(config.dataWidth bits)
+  val PREADY = Bool()
 
   override def asMaster(): Unit = {
-    // TODO define direction of each signal in a master mode
-
+    out(PSEL, PENABLE, PWRITE, PADDR, PWDATA)
+    in(PRDATA, PREADY)
   }
 }
 
-case class ApbPwm(apbConfig: ApbConfig,timerWidth : Int) extends Component {
+case class ApbPwm(apbConfig: ApbConfig, timerWidth: Int) extends Component {
   require(apbConfig.dataWidth == 32)
   require(apbConfig.selWidth == 1)
 
   val io = new Bundle{
-    val apb = ??? // TODO
-    val pwm = ??? // TODO
+    val apb = slave(Apb(apbConfig))
+    val pwm = out(Bool())
   }
 
   val logic = new Area {
-    // TODO define the PWM logic
+    val enable    = Reg(Bool()) init(False)
+    val timer     = Reg(UInt(timerWidth bits)) init(0)
+    val dutyCycle = Reg(UInt(timerWidth bits)) init(0)
+    val output    = Reg(Bool()) init(False)
+
+    when(enable === True) {
+      timer := timer + 1
+    }
+
+    when(timer === 0) {
+      output := True
+    }
+    when(timer === dutyCycle) {
+      output := False
+    }
+
+    io.pwm := output
   }
-  
+
   val control = new Area {
-    // TODO define the APB slave logic that will make PWM's registers writable/readable
+    val doWrite = io.apb.PSEL(0) && io.apb.PENABLE && io.apb.PWRITE
+
+    io.apb.PRDATA := 0
+    io.apb.PREADY := True
+
+    switch(io.apb.PADDR) {
+      is(0) {
+        io.apb.PRDATA(0) := logic.enable
+        when(doWrite) {
+          logic.enable := io.apb.PWDATA(0)
+        }
+      }
+      is(4) {
+        io.apb.PRDATA := logic.dutyCycle.asBits.resized
+        when(doWrite) {
+          logic.dutyCycle := io.apb.PWDATA.asUInt.resized
+        }
+      }
+    }
   }
+
+//  val control = new Area {
+//    io.apb.PRDATA := 0
+//    io.apb.PREADY := True
+//
+//    when(io.apb.PSEL(0) && io.apb.PENABLE) {
+//      when(io.apb.PWRITE) {
+//        // Write
+//        switch(io.apb.PADDR) {
+//          is(0) { logic.enable := io.apb.PWDATA(0) }
+//          is(4) { logic.dutyCycle := io.apb.PWDATA.asUInt.resized }
+//        }
+//      } otherwise {
+//        // Read
+//        io.apb.PRDATA := B"32'00000000"
+//        switch(io.apb.PADDR) {
+//          is(0) { io.apb.PRDATA(0) := logic.enable }
+//          is(4) { io.apb.PRDATA := logic.dutyCycle.asBits.resized }
+//        }
+//      }
+//    }
+//  }
 }
